bebae7e155bb5f00df9c180baf0141cc525ae649
azvinarti <azhvinarti12@gmail.com>
2024-03-07 09:20:22 +0700

    Update login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,8 @@
+<div>
+<html>
+    <h3><b>Login</b></h3>
+    <input type="text"><br>
+    <input type="password"><br><br>
+    <button>Login</button>
+</html>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ListBarangController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// routes/web.php
+route::get('/user/{id}', function ($id){
+    return 'User dengan ID'. $id;
+});
+
+// routes/web.php
+route::prefix('admin')->group(function (){
+    route::get('dashboard', function(){
+        return 'Admin Dashboard';
+    });
+
+    route::get('/users', function (){
+        return 'Admin Users';
+    });
+});
+
+// route::get('/listbarang/{id}/{nama}', function($id, $nama){
+//     return view('list_barang', compact('id', 'nama'));
+// });
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
+
+Route::get('/login', [LoginController::class, 'login']);
